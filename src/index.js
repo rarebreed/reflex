ae190c46952f86733cf7f6f1a1c41419793bc9a8
Sean Toner <stoner@localhost.localdomain>
2017-05-10 15:21:13 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const APP = () => {
+    return(
+            <div>
+              "hello world"
+            </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelect(".container"));
